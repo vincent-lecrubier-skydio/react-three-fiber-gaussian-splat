672a5dc31e0838de9af798e15c9f38ff1f442764
vincent-lecrubier-skydio <94416008+vincent-lecrubier-skydio@users.noreply.github.com>
2023-09-13 16:58:36 -0700

    Scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { OrbitControls, StatsGl } from '@react-three/drei';
+import { Environment, OrbitControls, StatsGl } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Box } from './box';
 import { Splat } from './splat-object';
-import { Suzi } from './suzi';
+import { OrbitingSuzi } from './suzi';
+import { useControls } from 'leva';
 
 function App() {
+  useControls({ name: 'World', aNumber: 0 });
   return (
     <Canvas className="h-full w-full bg-black" gl={{ antialias: false }}>
       <StatsGl />
       <OrbitControls />
-      <ambientLight />
-      <directionalLight position={[10, 10, 10]} />
       <Box position={[-1, 0, 0]} />
       <Box position={[1, 0, 0]} />
-      <Suzi position={[0, 0, 2]} />
-      <group position={[3, 0, 0]}>
+
+      <OrbitingSuzi />
+
+      <group position={[0, 0, 0]}>
         <Splat />
       </group>
+      <Environment preset="city" />
     </Canvas>
   );
 }
--- src/suzi.tsx
+++ src/suzi.tsx
@@ -1,12 +1,47 @@
 import { useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import { useLayoutEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { FlakesTexture } from 'three-stdlib';
 
-export const Suzi = (props) => {
-  const { nodes } = useGLTF(
+export const Suzi = ({ objref: ref, ...props }) => {
+  const { scene, materials } = useGLTF(
     'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/suzanne-high-poly/model.gltf'
   );
+  useLayoutEffect(() => {
+    scene.traverse((obj) => {
+      if (!obj.isMesh) return;
+      obj.receiveShadow = true;
+      obj.castShadow = true;
+      ref.current = obj;
+    });
+    materials.default.color.set('orange');
+    materials.default.roughness = 0;
+    materials.default.normalMap = new THREE.CanvasTexture(
+      new FlakesTexture(),
+      THREE.UVMapping,
+      THREE.RepeatWrapping,
+      THREE.RepeatWrapping
+    );
+    materials.default.normalMap.repeat.set(40, 40);
+    materials.default.normalScale.set(0.1, 0.1);
+  });
+
+  return <primitive object={scene} {...props} />;
+};
+
+export const OrbitingSuzi = () => {
+  const groupRef = useRef(null);
+  const meshRef = useRef(null);
+
+  useFrame((state, delta) => {
+    groupRef.current.rotateY(delta * 0.5);
+    meshRef.current.rotateY(delta * 5);
+  });
+
   return (
-    <mesh castShadow receiveShadow geometry={nodes.Suzanne.geometry} {...props}>
-      <meshStandardMaterial color="#ff00ff" roughness={0} />
-    </mesh>
+    <group ref={groupRef}>
+      <Suzi position={[0, 0, 1]} scale={0.5} objref={meshRef} />
+    </group>
   );
 };
