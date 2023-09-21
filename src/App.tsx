577e67f1ad0fa4e6a0a75f544e32b10c9ca5d0b5
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2023-09-20 20:55:39 -0700

    Tests on our own data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   Environment,
+  Gltf,
   OrbitControls,
   PerformanceMonitor,
   StatsGl,
@@ -9,9 +10,56 @@ import { Box } from './box';
 import { Splat } from './splat-object';
 import { OrbitingSuzi } from './suzi';
 import { Leva, useControls } from 'leva';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 
-const urls = [
+// import telco from "./telco.gltf";
+
+// import model30k from './model30k.splat';
+// import output from './output.splat';
+// import telco7k from './telco7k.splat';
+// import telco14k from './telco14k.splat';
+// import telco21k from './telco21k.splat';
+// import telco30k from './telco30k.splat';
+// import telco414k from './telco414k.splat';
+// import telco421k from './telco421k.splat';
+// import telco428k from './telco428k.splat';
+// import telco435k from './telco435k.splat';
+// import telco442k from './telco442k.splat';
+// import telco449k from './telco449k.splat';
+// import telco456k from './telco456k.splat';
+// import telco463k from './telco463k.splat';
+// import telco4200k from './telco4200k.splat';
+// import telco_2_7k from './telco-2-7k.splat';
+// import telco_2_14k from './telco-2-14k.splat';
+// import telco_2_21k from './telco-2-21k.splat';
+// import telco_2_28k from './telco-2-28k.splat';
+// import telco_2_42k from './telco-2-42k.splat';
+
+const gltfUrls = [
+  // telco
+] as const;
+
+const splatUrls = [
+  // telco_2_42k,
+  // telco_2_28k,
+  // telco_2_21k,
+  // telco_2_14k,
+  // telco_2_7k,
+  // telco4200k,
+  // telco463k,
+  // telco456k,
+  // telco449k,
+  // telco442k,
+  // telco435k,
+  // telco428k,
+  // telco421k,
+  // telco414k,
+  // telco30k,
+  // telco21k,
+  // telco14k,
+  // telco7k,
+  // output,
+  // model30k,
   'https://antimatter15.com/splat-data/train.splat',
   'https://antimatter15.com/splat-data/plush.splat',
   'https://antimatter15.com/splat-data/truck.splat',
@@ -20,12 +68,13 @@ const urls = [
   'https://antimatter15.com/splat-data/stump.splat',
   'https://antimatter15.com/splat-data/bicycle.splat',
   'https://media.reshot.ai/models/nike_next/model.splat',
-];
+] as const;
 
 function App() {
   // On screen controls
-  const { url, throttleDpr, maxDpr, throttleSplats, maxSplats } = useControls({
-    url: { label: 'Model URL', options: urls },
+  const { splatUrl, gltfUrl, throttleDpr, maxDpr, throttleSplats, maxSplats } = useControls({
+    splatUrl: { label: 'Splat URL', options: splatUrls },
+    gltfUrl: { label: "Gltf URL", options: gltfUrls },
     throttleDpr: {
       label: 'Degrade pixel ratio based on perf.',
       value: false,
@@ -36,7 +85,7 @@ function App() {
       value: false,
     },
     maxSplats: { label: 'Max splat count', value: 10000000 },
-  });
+  }) as any;
 
   // Performance factor
   const [factor, setFactor] = useState(1);
@@ -53,6 +102,10 @@ function App() {
     ? Math.min(maxSplats, splats)
     : maxSplats;
 
+  const splatScale = 2.7 as number;
+  const splatPos = [12.1 + 0, 19.3, -1.0] as [number, number, number];
+  const splatRot = [-0.516, 0.15, 0.1] as [number, number, number];
+
   return (
     <>
       <Leva oneLineLabels collapsed />
@@ -67,7 +120,7 @@ function App() {
           step={1}
           onIncline={({ factor }) => {
             setFactor(factor);
-            setSplats(
+            setSplats(() =>
               Math.min(
                 maxSplats,
                 Math.round((0.9 + 0.2 * factor) * effectiveSplats)
@@ -76,7 +129,7 @@ function App() {
           }}
           onDecline={({ factor }) => {
             setFactor(factor);
-            setSplats(
+            setSplats(() =>
               Math.min(
                 maxSplats,
                 Math.round((0.9 + 0.2 * factor) * effectiveSplats)
@@ -94,9 +147,15 @@ function App() {
 
         <OrbitingSuzi />
 
-        <group position={[0, 0, 0]}>
-          <Splat url={url} maxSplats={effectiveSplats} />
-        </group>
+        {gltfUrl && <Suspense fallback={null}>
+          <group position={[2, 0, 0]} rotation={[0, 0, 0]}  >
+            <Gltf src={gltfUrl} />
+          </group>
+        </Suspense>}
+
+        {splatUrl && <group position={splatPos} rotation={splatRot} scale={[splatScale, splatScale, splatScale]} >
+          <Splat url={splatUrl} maxSplats={effectiveSplats} />
+        </group>}
         <Environment preset="city" />
       </Canvas>
       <div className="absolute bottom-0 left-0 rounded-lg bg-white shadow border-gray-200 bg-white p-2 m-4">
